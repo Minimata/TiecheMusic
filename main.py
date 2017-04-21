f5ed79b67344cfb3d85af99ded23836e443dc5eb
arnaud droxler <arnaud.droxler@gmail.com>
2017-04-21 16:43:25 +0200

    dection des note

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 import matplotlib.pyplot as plt
 import math
 import numpy as np
+import sys
 
 
 def img_to_binary_grey_scale(img):
@@ -45,67 +46,6 @@ def show_hist(hist, title="histogram"):
     plt.grid(True)
     plt.show()
 
-
-def crop_sheet_in_scope(img):
-    hist = img_binary_vertical_hist(img)
-    lines = []
-    for index, val in enumerate(hist):
-        if val > 600:
-            lines.append(index)
-
-    number_scope = 0
-    scope_first_line = []
-    for i, line in enumerate(lines):
-        if i % 5 == 0:
-            number_scope += 1
-            scope_first_line.append(line)
-
-    size_scope = int(abs(lines[0] - lines[4]))
-    offset_scope = int(abs(math.ceil(lines[4] - lines[5]) / 6))
-    width, height = img.shape[:2]
-
-    scope_list = []
-    for i in range(0, number_scope):
-
-        x = scope_first_line[i] - offset_scope
-        h_x = size_scope + 2 * offset_scope
-
-        scope_list.append(img[x:x + h_x, 0:width])
-
-    return scope_list
-
-
-def crop_line_in_note(line):
-    width, height = line.shape[:2]
-    hist = img_binary_horizontal_hist(line)
-    hist_reduced = []
-    for index, val in enumerate(hist):
-        if val >= 5 and val <= 15:
-            hist_reduced.append((index,val))
-
-    rising_edge = True
-    rising_edge_list = []
-    falling_edge_list = []
-    for index, val in enumerate(hist_reduced):
-        try:
-            if hist_reduced[index + 1][1] > 5.0 and val[1] == 5.0 and rising_edge:
-                rising_edge_list.append(val[0])
-                rising_edge = False
-            elif val[1] > 5.0 and hist_reduced[index + 1][1] == 5.0 and not rising_edge:
-                falling_edge_list.append(val[0])
-                rising_edge = True
-        except IndexError:
-            pass
-
-    note_list = []
-    for i in range(0, len(rising_edge_list)):
-        distance = falling_edge_list[i] - rising_edge_list[i]
-        if distance < 8.0:
-            middle_top = rising_edge_list[i]+ math.ceil(distance/2.0)
-            x = middle_top - distance
-            h_x = middle_top + distance
-            note_list.append(line[0:height, x:h_x])
-    return note_list
 
 def extract_notes(binary_image):
     # cv2.imshow('originale', binary_image)
@@ -172,18 +112,115 @@ def show_image(image):
     cv2.destroyAllWindows()
 
 
-def print_note(note_list):
-    for i in range(0, len(note_list)):
-        cv2.imshow("couco", note_list[i])
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+def crop_sheet_in_scope(img):
+    hist = img_binary_vertical_hist(img)
+    lines = []
+    for index, val in enumerate(hist):
+        if val > 600:
+            lines.append(index)
+
+    number_scope = 0
+    scope_first_line = []
+    for i, line in enumerate(lines):
+        if i % 5 == 0:
+            number_scope += 1
+            scope_first_line.append(line)
+
+    size_scope = int(abs(lines[0] - lines[4]))
+    offset_scope = int(abs(math.ceil(lines[4] - lines[5]) / 6))
+    width, height = img.shape[:2]
+
+    scope_list = []
+    for i in range(0, number_scope):
+
+        x = scope_first_line[i] - offset_scope
+        h_x = size_scope + 2 * offset_scope
+
+        scope_list.append(img[x:x + h_x, 0:width])
+
+    return scope_list
+
+
+def crop_line_in_note(line):
+    width, height = line.shape[:2]
+    hist = img_binary_horizontal_hist(line)
+    hist_reduced = []
+    for index, val in enumerate(hist):
+        if val >= 5 and val <= 15:
+            hist_reduced.append((index,val))
+
+    rising_edge = True
+    rising_edge_list = []
+    falling_edge_list = []
+    for index, val in enumerate(hist_reduced):
+        try:
+            if hist_reduced[index + 1][1] > 5.0 and val[1] == 5.0 and rising_edge:
+                rising_edge_list.append(val[0])
+                rising_edge = False
+            elif val[1] > 5.0 and hist_reduced[index + 1][1] == 5.0 and not rising_edge:
+                falling_edge_list.append(val[0])
+                rising_edge = True
+        except IndexError:
+            pass
+
+    note_list = []
+    for i in range(0, len(rising_edge_list)):
+        distance = falling_edge_list[i] - rising_edge_list[i]
+        if distance < 8.0:
+            middle_top = rising_edge_list[i]+ math.ceil(distance/2.0)
+            x = middle_top - distance
+            h_x = middle_top + distance
+            note_list.append(line[0:height, x:h_x])
+    return note_list
+
+
+def detect_note(note_list):
+    for note in note_list:
+        hist = img_binary_vertical_hist(note)
+
+        lines = []
+        note = 0
+        for index, val in enumerate(hist):
+            if val > 10:
+                lines.append(index)
+            if val >= 3 and hist[index-1] >= 3 and hist[index+1] >= 3 and hist[index-2] >= 3 and hist[index+2] >= 3:
+                note = index
+
+        try:
+            if note < lines[0]:
+                print("SOL 5", end=" ")
+            elif note == lines[0]:
+                print("FA 5", end=" ")
+            elif note > lines[0] and note < lines[1]:
+                print("MI 5", end=" ")
+            elif note == lines[1]:
+                print("RE 5", end=" ")
+            elif note > lines[1] and note < lines[2]:
+                print("DO 5", end=" ")
+            elif note == lines[2]:
+                print("SI 4", end=" ")
+            elif note > lines[2] and note < lines[3]:
+                print("LA 4", end=" ")
+            elif note == lines[3]:
+                print("SOL 4", end=" ")
+            elif note > lines[3] and note < lines[4]:
+                print("FA 4", end=" ")
+            elif note == lines[4]:
+                print("MI 4", end=" ")
+            elif note > lines[4]:
+                print("Re 4", end=" ")
+        except IndexError:
+            pass
+        print("\t", end=" ")
+
 
 
 def histogramm_process(binary):
     scope_list = crop_sheet_in_scope(binary)
     for line in scope_list:
         note_list = crop_line_in_note(line)
-        print_note(note_list)
+        detect_note(note_list)
+        print()
 
 
 def morpho_process(binary):
@@ -195,8 +232,8 @@ def main():
     img = cv2.imread(image)
     binary = img_to_binary_grey_scale(img)
     #choose the process
-    #histogramm_process(binary)
-    morpho_process(binary)
+    histogramm_process(binary)
+    #morpho_process(binary)
 
 
 if __name__ == '__main__':
